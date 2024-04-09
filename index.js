3ca7a9cc24cea27c230e33d10fa9ad1ecd1aff2d
AkshDZN <129135009+AkshDZN@users.noreply.github.com>
2024-04-09 22:07:50 +0530

    fixed location issue on mobile

--- index.js
+++ index.js
@@ -90,4 +90,25 @@ function toggler() {
     getLocation();
 }
 
+function handlePermission() {
+    navigator.permissions.query({ name: "geolocation" }).then((result) => {
+      if (result.state === "granted") {
+        report(result.state);
+      } else if (result.state === "prompt") {
+        report(result.state);
+      } else if (result.state === "denied") {
+        report(result.state);
+      }
+      result.addEventListener("change", () => {
+        report(result.state);
+      });
+    });
+  }
+  
+  function report(state) {
+    console.log(`Permission ${state}`);
+  }
+  
+  handlePermission();
+
 
